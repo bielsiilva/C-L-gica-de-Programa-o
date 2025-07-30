6314ac4b27cfae8d1d40fe615691dc2c4fbc9ccc
Gabriel Silva <62155272+bielsiilva@users.noreply.github.com>
2025-07-29 23:11:14 -0300

    Add files via upload

---
+++ ExerciciosCondicional/ExerciciosCondicional/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExerciciosCondicional
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, duracao;
+
+            string[] vet = Console.ReadLine().Split(' ');
+            a = int.Parse(vet[0]);
+            b = int.Parse(vet[1]);
+            
+
+            if (a < b)
+            {
+                duracao = b - a;
+            }
+            else
+            {
+                duracao = 24 - a + b;
+            }
+
+            Console.WriteLine($"O JOGO DUROU {duracao} HORA(S)");
+
+        }
+    }
+}
