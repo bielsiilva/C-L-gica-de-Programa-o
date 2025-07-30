ed25b40cf720b150cdc18e8fc03b0f74b0c4694f
Gabriel Silva <62155272+bielsiilva@users.noreply.github.com>
2025-07-29 23:10:10 -0300

    Add files via upload

---
+++ EstruturaFor/EstruturaFor/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Globalization;
+
+namespace EstruturaFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, min, max, soma = 0;
+
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+
+            if (x < y)
+            {
+                min = x;
+                max = y;
+            }
+            else
+            {
+                min = y;
+                max = x;
+            }
+
+            for (int i = min + 1; i < max; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    soma = soma + i;
+                }
+            }
+
+            Console.WriteLine($"{soma}");
+        }
+    }
+}
+
+/*
+            int valor, total;
+
+            valor = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                total = i * valor;
+
+                Console.WriteLine($"{i} x {valor} = {total}");
+            }
+            */
+
+/*
+            int soma = 0, i;
+            int x = int.Parse(Console.ReadLine());
+
+            for (i = 0; i < x; i++)
+            {
+                int y = int.Parse(Console.ReadLine());
+                soma += y;
+            }
+
+            Console.WriteLine($"{soma}");
+            */
